df0701f0c68ed1506c646644ebc90be9e05949f3
Amanpathan26 <amanpaathan@gmail.com>
2025-01-20 16:14:45 +0530

    cmy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
     domains: ["coin-images.coingecko.com", "https://cdn.photoroom.com/v2/image-cache?path=gs://background-7ef44.appspot.com/backgrounds_v3/black/47_-_black.jpg"],
   },
